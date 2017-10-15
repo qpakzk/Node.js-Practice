d45dec839607bbb300de1d029504bed66acaf1f9
Sangwon Hong <qpakzk@gmail.com>
2017-10-16 04:19:53 +0900

    start to implement app.js
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.send('Hello Home Page');
+});
+
+app.get('/dynamic', function(req, res) {
+	var time = Date();
+	var lis = '';
+	for(var i = 0; i < 5; i++) {
+		lis = lis + '<li>coding</li>';
+	}
+
+	var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+          ${lis}
+        </ul>
+        ${time}
+    </body>
+  </html>`;
+
+  res.send(output);
+});
+app.get('/route', function(req, res) {
+	res.send('Hello Router, ' + '<img src="/sample.png">');
+});
+app.get('/login', function(req, res) {
+	res.send('<h1>Login please</h1>');
+});
+app.listen(3000, function() {
+	console.log('Connected 3000 port!');
+});
