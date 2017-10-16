e9a95d1130ad95edb804c5effd209a872998529e
Sangwon Hong <qpakzk@gmail.com>
2017-10-16 17:18:28 +0900

    develop file uploading
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- app_file.js
+++ app_file.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var app = express();
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({ storage: storage });
 var fs = require('fs');
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/user', express.static('uploads'));
 app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'jade');
+
+app.get('/upload', function(req, res) {
+	res.render('upload');
+});
+
+app.post('/upload', upload.single('userfile'), function(req, res) {
+	res.send('Uploaded:' + req.file.filename);
+});
 
 app.get('/topic/new', function(req, res) {
 
