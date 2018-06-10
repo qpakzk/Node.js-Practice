0d4557982e8e25c75ff3b6064af6d003a6257a17
Sangwon Hong <qpakzk@gmail.com>
2018-06-10 19:56:32 +0900

    Implement dynamic using router
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Hello home page!');
+});
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Login please...</h1>');
+});
+
+app.get('/dynamic', (req, res) => {
+    var lis = '';
+    for(i = 0; i < 10; i++)
+        lis += ('<li>coding ' + (i + 1) +'</li>');
+    var time = Date();
+    var output = `    
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Dynamic</title>
+    </head>
+    <body>
+        Hello, dynamic!!!
+        <ul>
+            ${lis}
+        </ul>
+        ${time}
+    </body>
+    </html>
+    `;
+    res.send(output);
+})
+app.get('/route', (req, res) => {
+    res.send('Hello Router, <img src="/route.png">');
+});
+
+app.listen(3000, () => {
+    console.log("Connected 3000 port!");
+});
